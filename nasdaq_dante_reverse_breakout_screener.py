b51cf717e49d79176ca056c1e434be53b248095f
51552462 <zhdaos15@naver.com>
2026-03-13 12:17:57 +0900

    Update nasdaq_dante_reverse_breakout_screener.py

--- nasdaq_dante_reverse_breakout_screener.py
+++ nasdaq_dante_reverse_breakout_screener.py
@@ -399,20 +399,21 @@ def run_scheduler():
             print(f"🚀 [US 역매공파/1H 정규 스캔 시작] 미국 현지시간: {now_ny.strftime('%Y-%m-%d %H:%M:%S')}")
             scan_market('1h')
             print("💤 1H 스캔 완료. 다음 타임까지 대기합니다...")
-            time.sleep(50 * 60) 
+            time.sleep(60) 
             
         # 💡 [시간 분산] 미장 마감 20분 뒤 (16:20) 일봉 검사
         elif now_ny.hour == 14 and now_ny.minute == 20:
             print(f"🚀 [US 정배열/1D 정규 스캔 시작] 미국 현지시간: {now_ny.strftime('%Y-%m-%d %H:%M:%S')}")
             scan_market('1d')
             print("💤 1D 스캔 완료. 내일 개장까지 대기합니다...")
-            time.sleep(60)
+            time.sleep(50 * 60) 
             
         else:
             time.sleep(10)
 
 if __name__ == "__main__":
     run_scheduler()
+
 
 
 
