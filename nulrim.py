c2b92a210b83f56fd015e2a5d25c7ecdf21a8845
51552462 <zhdaos15@naver.com>
2026-04-27 18:17:59 +0900

    Update nulrim.py

--- nulrim.py
+++ nulrim.py
@@ -559,6 +559,7 @@ def compute_signal(df_raw: pd.DataFrame, idx_close: pd.Series, marcap: float, co
     match_similarity = 0.0
     matched_name = ""
     best_window = 150
+    max_sn_similarity = 0.0
 
     try:
         # 💡 [핵심] 30, 60, 90, 120, 150, 180, 210일 7가지 렌즈로 시계열 압축/팽창 탐색
