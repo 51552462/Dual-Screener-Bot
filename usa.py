7375e84a72cd7c6d90459fc1d7f49ce45b335604
51552462 <zhdaos15@naver.com>
2026-04-30 21:58:19 +0900

    Update usa.py

--- usa.py
+++ usa.py
@@ -29,7 +29,7 @@ warnings.filterwarnings('ignore')
 logging.getLogger('yfinance').setLevel(logging.CRITICAL)
 
 # 💡 1. 듀얼 텔레그램 봇 세팅 (본캐용 / 홍보용 분리)
-TELEGRAM_TOKEN_MAIN  = "7791873924:AAHcaajPux8r0KVydUqpQjaqAeYlwxrZ7tg"
+TELEGRAM_TOKEN_MAIN  = "7791873924:AAElFAy66rBbM-cgC3QW_jJ1hnBOu8bZV-I"
 TELEGRAM_TOKEN_PROMO = "7996581031:AAFou3HWYhIXzRtlW4ildx8tOitcQBVubPg"
 TELEGRAM_CHAT_ID     = "6838834566"
 SEND_TELEGRAM        = True
