c9f5eb9e768c1958736a40892e0eb3f8f573d152
51552462 <zhdaos15@naver.com>
2026-05-03 18:27:56 +0900

    Update system_auto_pilot.py

--- system_auto_pilot.py
+++ system_auto_pilot.py
@@ -14,7 +14,7 @@ warnings.filterwarnings('ignore')
 # ==========================================
 # 💡 [환경 설정]
 # ==========================================
-TELEGRAM_TOKEN_MAIN = "7988939051:AAH18gmMs9syze2g4zo7Xd2stMdyREg66rI"
+TELEGRAM_TOKEN_MAIN = "8709452406:AAHGVhTN8hu1ujA_xYUR8GvMPrd-qpMoSRk"
 TELEGRAM_CHAT_ID    = "6838834566"
 DB_PATH = os.path.join(os.path.expanduser('~'), 'dante_bots', 'Dual-Screener-Bot', 'market_data.sqlite')
 CONFIG_PATH = os.path.join(os.path.expanduser('~'), 'dante_bots', 'Dual-Screener-Bot', 'system_config.json')
